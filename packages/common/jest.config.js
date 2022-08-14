da12ba3f648bf0ce8b9ed945904d829e80d99203
Michael Cesare <michaelborg1992@gmail.com>
2022-08-14 08:24:17 +0200

    fixed test

--- packages/common/jest.config.js
+++ packages/common/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
+  roots: ["<rootDir>/src"],
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transform: {
--- packages/common/src/utils/dateTime.util.test.ts
+++ packages/common/src/utils/dateTime.util.test.ts
@@ -3,15 +3,12 @@ import { differenceInMS } from './dateTime.util';
 describe( 'dateTimeUtil-differenceInMS', () => {
   it( 'should return positive', () => {
     const date1 = new Date();
-    date1.setHours( 1 );
-    date1.setMinutes( 12 );
-    date1.setSeconds( 1 );
+    date1.setHours( 1, 12, 1, 0 );
     const date2 = new Date();
-    date2.setHours( 1 );
-    date2.setMinutes( 12 );
-    date2.setSeconds( 2 );
-
-    expect( differenceInMS( date1, date2 ) ).toBe( 60 );
+    date2.setHours( 1, 12, 2, 0 );
+    const expected = 1 * 1000;
+    const actual = differenceInMS( date1, date2 );
+    expect( actual ).toBe( expected );
   } );
 
   it('example spec', () => {
